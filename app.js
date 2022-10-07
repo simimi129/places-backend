2cc85936265e7c1a4485efdcfe4a171f790ca74b
simimi129 <simimi129@gmail.com>
2022-10-07 11:15:17 +0200

    adding basic routes

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const placesRoutes = require("./routes/places-routes");
+
 const app = express();
+
+app.use("/api/places", placesRoutes);
+
+app.use((error, req, res, next) => {});
 
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,3 +1,49 @@
 const express = require("express");
 
 const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "title1",
+    description: "description1",
+    location: {
+      lat: 10,
+      lng: 10,
+    },
+    address: "address1",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    return res
+      .status(404)
+      .json({ message: "Could not find a place for the provided place id." });
+  }
+
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return res
+      .status(404)
+      .json({ message: "Could not find a place for the provided user id." });
+  }
+
+  res.json({ place });
+});
+
+module.exports = router;
